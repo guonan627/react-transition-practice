b04d60d32366c8e6435bed6c5a1eb6da18417e83
Nan Guo <guonan627@hotmail.com>
2021-09-20 16:56:52 +1000

    click show and hide

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import './style.css';
 
 class App extends Component {
-  render(){
-    return <div>hello</div>
+
+  constructor(props){
+    super(props);
+    this.state = {
+      show: true
+    }
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  render() {
+    return (
+    <Fragment>
+      <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+      <button onClick={this.handleToggle}>toggle</button>
+    </Fragment>
+    )
+  }
+
+  handleToggle() {
+    this.setState({
+      show: this.state.show ? false : true
+    })
   }
 }
 
