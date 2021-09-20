3b1fd88ef2bb05c6b1e8d8f7dac9e1106f77b5cb
Nan Guo <guonan627@hotmail.com>
2021-09-20 23:15:04 +1000

    css-transition-group

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component, Fragment} from 'react';
-import './style.css';
+import { CSSTransition } from 'react-transition-group';
+// import './style.css';
+import './csstransition.css';
 
 class App extends Component {
 
@@ -13,10 +15,24 @@ class App extends Component {
 
   render() {
     return (
-    <Fragment>
-      <div className={this.state.show ? 'show' : 'hide'}>hello</div>
-      <button onClick={this.handleToggle}>toggle</button>
-    </Fragment>
+      <Fragment>
+        {/* <div className={this.state.show ? 'show' : 'hide'}>hello</div> */}
+        {/* 引入了cssTransition之后， 包裹住写成 */}
+        <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames='fade'
+          unmountOnExit 
+          // 隐藏之后dom被移除
+          onEntered={(el)=> {el.style.color='red'}}
+          //在入场动画结束之后 onEntered会被自动执行
+          // el参数指的就是<div>hello</div>这个元素
+          appear={true}
+        >
+          <div>hello</div>
+        </CSSTransition>
+        <button onClick={this.handleToggle}>toggle</button>
+      </Fragment>
     )
   }
 
